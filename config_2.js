fb548e094a5874bf889553f409d1afb5c1ca81c9
DeepoweRs <bedirhan_kurt_@outlook.com>
2024-10-25 13:09:40 +0200

    2 practice project
    First 2 product card practice projects.

---
+++ config_2.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                yellow: "#EDC42C",
+                black: "#2D2E32",
+                inputBlack: "#26272A",
+                textGray: "#BDBCBC"
+            },
+            fontFamily: {
+                roboto: ["Roboto Condensed", "sans-serif"],
+                noto: ["Noto Sans TC", "sans-serif"],
+            }
+        }
+    }
+}
